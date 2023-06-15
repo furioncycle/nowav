d38eb1807dd3d227633f6eca87c14def2d24f976
Angel Marquez <admarquez21@gmail.com>
2023-06-14 18:52:02 -0700

    modified build.zig to allow modules

--- build.zig
+++ build.zig
@@ -1,22 +1,5 @@
 const std = @import("std");
 
-var _module: ?*std.build.Module = null;
-
-pub fn module(b: *std.Build) *std.build.Module {
-    if (_module) |m| return m;
-    _module = b.createModule(.{
-        .source_file = .{ .path = sdkPath("/src/main.zig") },
-    });
-    return _module.?;
-}
-
-fn sdkPath(comptime suffix: []const u8) []const u8 {
-    if (suffix[0] != '/') @compileError("suffix must be an absolute path");
-    return comptime blk: {
-        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
-        break :blk root_dir ++ suffix;
-    };
-}
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
@@ -32,24 +15,10 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // const zigclap = b.dependency("zigclap", .{
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-
-    const lib = b.addStaticLibrary(.{
-        .name = "nowav",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+    const nowav_deps = b.addModule("nowav", .{
+        .source_file = .{ .path = "src/main.zig" },
     });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    _ = nowav_deps;
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
