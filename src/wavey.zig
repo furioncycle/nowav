e8d3df5a72e3d7e898b7960a4a94a22e6e9ae196
Angel Marquez <admarquez21@gmail.com>
2023-06-18 23:26:12 -0700

    Fixed encoding to update file length, decode now handles fmt and data chunks

--- src/wavey.zig
+++ src/wavey.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
+const io = std.io;
 const testing = std.testing;
 
 const WaveErrors = error{
+    NoRiffChunk,
+    NoWaveChunk,
+    NoDataChunk,
     NoChunk,
 };
 
@@ -22,13 +26,12 @@ const Ids = enum {
 };
 
 const WaveHeader = struct {
-    length: u32,
     fmt: Fmt,
     data: Data,
     fact: ?Fact = null,
     pub fn decode(reader: anytype) !WaveHeader {
         if (!(try parseId("RIFF", reader))) {
-            return WaveErrors.NoChunk;
+            return WaveErrors.NoRiffChunk;
         }
 
         const fileLen = try reader.readInt(
@@ -37,29 +40,28 @@ const WaveHeader = struct {
         );
 
         if (!(try parseId("WAVE", reader))) {
-            return WaveErrors.NoChunk;
+            return WaveErrors.NoWaveChunk;
         }
 
         var buf: [4]u8 = undefined;
         var counter: usize = fileLen;
         var len: usize = 0;
-        _ = try reader.read(&buf);
 
         var w: WaveHeader = undefined;
 
         while (counter > 0) : (counter -= len) {
+            _ = try reader.read(&buf);
             switch (Ids.toEnum(&buf)) {
                 .Fmt => {
                     w.fmt = try Fmt.decode(reader);
-                    len = w.length;
+                    len = w.fmt.length + @sizeOf([4]u8);
                 },
                 .Data => {
                     w.data = try Data.decode(reader);
-                    len = w.length;
+                    len = w.data.length + @sizeOf([4]u8);
                 },
                 .Fact => {
                     w.fact = try Fact.decode(reader);
-                    len = w.length;
                 },
             }
         }
@@ -116,17 +118,14 @@ const Data = struct {
     length: u32 = 0,
 
     fn decode(reader: anytype) !Data {
-        if (!(try parseId("data", reader))) {
-            return WaveErrors.NoChunk;
-        }
         return Data{
             .length = try reader.readInt(u32, .Little),
         };
     }
 };
 
-pub fn decode(allocator: std.mem.Allocator, reader: anytype) !Decoder(@TypeOf(reader)) {
-    return Decoder(@TypeOf(reader)).init(allocator, reader);
+pub fn decode(allocator: std.mem.Allocator, file: std.fs.File, reader: anytype) !Decoder(@TypeOf(reader)) {
+    return Decoder(@TypeOf(reader)).init(allocator, file, reader);
 }
 
 pub fn Decoder(comptime ReaderType: type) type {
@@ -134,12 +133,14 @@ pub fn Decoder(comptime ReaderType: type) type {
         const Self = @This();
 
         allocator: std.mem.Allocator,
-        inner_reader: ReaderType,
         header: WaveHeader,
+        file: std.fs.File,
+        inner_reader: ReaderType,
 
-        pub fn init(allocator: std.mem.Allocator, in_reader: ReaderType) !Self {
+        pub fn init(allocator: std.mem.Allocator, file: std.fs.File, in_reader: ReaderType) !Self {
             return Self{
                 .allocator = allocator,
+                .file = file,
                 .inner_reader = in_reader,
                 .header = try WaveHeader.decode(in_reader),
             };
@@ -154,65 +155,73 @@ fn parseId(id: []const u8, reader: anytype) !bool {
     return std.mem.eql(u8, &buf, id);
 }
 
-pub fn encode(allocator: std.mem.Allocator, writer: anytype) !Encode(@TypeOf(writer)) {
-    return Encode(@TypeOf(writer)).init(allocator, writer);
+pub fn encode(allocator: std.mem.Allocator, file: std.fs.File, writer: anytype) !Encode(@TypeOf(writer)) {
+    return Encode(@TypeOf(writer)).init(allocator, file, writer);
 }
 
 pub fn Encode(comptime WriterType: type) type {
     return struct {
         const Self = @This();
-
         allocator: std.mem.Allocator,
+        file: std.fs.File,
         inner_writer: WriterType,
 
-        pub fn init(allocator: std.mem.Allocator, in_writer: WriterType) !Self {
+        pub fn init(allocator: std.mem.Allocator, file: std.fs.File, in_writer: WriterType) !Self {
             return Self{
                 .allocator = allocator,
+                .file = file,
                 .inner_writer = in_writer,
             };
         }
 
-        pub fn writeFmt(self: Self, fmt: Fmt) !void {
+        pub fn writeFmt(self: Self, fmt: Fmt) !usize {
             _ = try self.inner_writer.write(&fmt.id);
-            _ = try self.inner_writer.writeInt(u32, fmt.length, .Little);
-            _ = try self.inner_writer.writeInt(u16, @enumToInt(fmt.audioFormat), .Little);
-            _ = try self.inner_writer.writeInt(u16, @enumToInt(fmt.numChannels), .Little);
-            _ = try self.inner_writer.writeInt(u32, fmt.sampleRate, .Little);
-            _ = try self.inner_writer.writeInt(u32, fmt.byteRate, .Little);
-            _ = try self.inner_writer.writeInt(u16, fmt.blockAlign, .Little);
-            _ = try self.inner_writer.writeInt(u16, fmt.bitsPerSample, .Little);
+            try self.inner_writer.writeInt(u32, fmt.length, .Little);
+            try self.inner_writer.writeInt(u16, @enumToInt(fmt.audioFormat), .Little);
+            try self.inner_writer.writeInt(u16, @enumToInt(fmt.numChannels), .Little);
+            try self.inner_writer.writeInt(u32, fmt.sampleRate, .Little);
+            try self.inner_writer.writeInt(u32, fmt.byteRate, .Little);
+            try self.inner_writer.writeInt(u16, fmt.blockAlign, .Little);
+            try self.inner_writer.writeInt(u16, fmt.bitsPerSample, .Little);
+            return fmt.length + @sizeOf([4]u8);
         }
 
-        pub fn writeData(self: Self, data: Data) !void {
+        pub fn writeData(self: Self, data: Data) !usize {
             _ = try self.inner_writer.write(&data.id);
-            _ = try self.inner_writer.writeInt(u32, data.length, .Little);
+            try self.inner_writer.writeInt(u32, data.length, .Little);
+            return @sizeOf([4]u8) + data.length;
         }
 
         pub fn writeHeader(self: Self, header: WaveHeader) !usize {
             var RIFF = [_]u8{ 'R', 'I', 'F', 'F' };
             var WAVE = [_]u8{ 'W', 'A', 'V', 'E' };
             _ = try self.inner_writer.write(&RIFF);
-            _ = try self.inner_writer.writeInt(u32, header.length, .Little);
+            try self.inner_writer.writeInt(u32, 0, .Little);
             _ = try self.inner_writer.write(&WAVE);
 
-            try self.writeFmt(header.fmt);
-            try self.writeData(header.data);
+            const fmtLen = try self.writeFmt(header.fmt);
+            const dataLen = try self.writeData(header.data);
 
-            return header.length;
+            //Go back to write the file length
+            try self.file.seekTo(RIFF.len);
+            try self.inner_writer.writeInt(u32, @intCast(u32, fmtLen) + @intCast(u32, dataLen), .Little);
+
+            return fmtLen + dataLen;
         }
     };
 }
+
 test "Read empty wave file" {
     const file = try std.fs.cwd().openFile("testSamples/doug.wav", .{});
     defer file.close();
 
-    try testing.expect(decode(testing.allocator, file.reader()) == WaveErrors.NoChunk);
+    try testing.expect(decode(testing.allocator, file, file.reader()) == WaveErrors.NoRiffChunk);
 }
 
 test "Parse Header" {
     const h: WaveHeader = .{
-        .length = 0,
         .fmt = .{
+            .length = @sizeOf(Fmt),
             .audioFormat = .Pcm,
             .numChannels = .Mono,
             .sampleRate = 44100,
@@ -220,7 +229,9 @@ test "Parse Header" {
             .blockAlign = 1 * 16 / 8,
             .bitsPerSample = 16,
         },
-        .data = .{},
+        .data = .{
+            .length = 0,
+        },
     };
 
     {
@@ -229,14 +240,14 @@ test "Parse Header" {
         });
         defer file.close();
 
-        const Wavey = try encode(testing.allocator, file.writer());
-        try testing.expect((try Wavey.writeHeader(h)) == 0);
+        const Wavey = try encode(testing.allocator, file, file.writer());
+        const v = try Wavey.writeHeader(h);
+        try testing.expect(v == @sizeOf(WaveHeader));
     }
 
     const file = try std.fs.cwd().openFile("testSamples/dougRiff.wav", .{});
     defer file.close();
-
-    const Wavey = try decode(testing.allocator, file.reader());
+    const Wavey = try decode(testing.allocator, file, file.reader());
 
     try testing.expectEqualDeep(h, Wavey.header);
 }
